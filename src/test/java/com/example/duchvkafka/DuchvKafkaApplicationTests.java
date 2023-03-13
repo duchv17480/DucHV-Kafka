e83656c57c5b26c20bbcf966c84bc1a44c91f0f8
duchvph17480 <hoangvietduc34hd@gmail.com>
2023-03-13 21:47:08 +0700

    first commit

---
+++ src/test/java/com/example/duchvkafka/DuchvKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.duchvkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuchvKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
